5730cddbd2facaed587bb806cc967d6f764e650d
Opscale <development@opscale.co>
2025-10-03 17:17:08 -0600

    perf(serviceprovider): using nova-package-tools

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -2,25 +2,28 @@
 
 namespace Opscale\NovaCatalogs;
 
-use Opscale\NovaCatalogs\Configuration\NovaPackage;
-use Opscale\NovaCatalogs\Configuration\NovaPackageServiceProvider;
 use Opscale\NovaCatalogs\Nova\Catalog;
 use Opscale\NovaCatalogs\Nova\CatalogItem;
+use Opscale\NovaPackageTools\NovaPackageServiceProvider;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 
 class ToolServiceProvider extends NovaPackageServiceProvider
 {
-    public function newPackage(): Package
-    {
-        return new NovaPackage;
-    }
-
     public function configurePackage(Package $package): void
     {
         $package
             ->name('nova-catalogs')
             ->discoversMigrations()
             ->runsMigrations()
-            ->hasResources([Catalog::class, CatalogItem::class]);
+            ->hasResources([
+                Catalog::class,
+                CatalogItem::class,
+            ])
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->askToRunMigrations()
+                    ->askToStarRepoOnGitHub('opscale-co/nova-catalogs');
+            });
     }
 }
